f74a5da8c910fafe23a7ccf2275c30ee124dd487
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-06-27 20:51:45 +0500

    taxminiy controller  tugadi. statik chiqarildi.

--- src/sections/Controller.jsx
+++ src/sections/Controller.jsx
@@ -1,83 +1,92 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { setAllContr } from "../features/counter/counterSlice";
 
 const Controller = () => {
-    const { books, controller, units } = useSelector((state) => state.counter);
-
-    const [contr, setContr] = useState({});
-    const [bookId, setBookId] = useState([]);
     const dispatch = useDispatch();
     dispatch(setAllContr("ok"));
-    function handlerBook(e) {
-        if (e.target.value) {
-            books.forEach(book => {
-                if (book.bookname == e.target.value) {
-                    bookId.push(book._id);
-                    setBookId([...bookId]);
-                }
-            })
-        }
-    }
+    const [contr, setContr] = useState({});
 
-    // {
-    //     units.map((unit) => {
-    //         {
-    //             bookId.map((bookId) => {
-    //                 if (bookId == unit._id) {
-    //                     // return <option key={unit._id} className="form-control">{unit.unitname}</option>
-    //                 }
-    //             })
-    //         }
-    //     })
-    // }
-    function handlerUnit(e) {
-        if (e.target.value) {
-            // books.forEach(book => {
-            //     if (book.bookname == e.target.value) {
-            //         bookId.push(book._id);
-            //         setBookId([...bookId]);
-            //     }
-            // })
+    const { books, controller, units } = useSelector((state) => state.counter);
+    const [selectedBooks, setSelectedBooks] = useState([]);
+    const [selectedUnits, setSelectedUnits] = useState([]);
+    const [wordsCount, setWordsCount] = useState(0);
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        // Seçili birimler veya kitaplar değiştiğinde wordsCount'u güncelle
+        let count = 0;
+        selectedUnits.forEach(unitId => {
+            const unit = units.find(unit => unit._id === unitId);
+            count += unit.words.length;
+        });
+        setWordsCount(count);
+    }, [selectedUnits, selectedBooks]);
+
+    const handleBookCheckboxChange = (event, bookId) => {
+        if (event.target.checked) {
+            setSelectedBooks(prevSelectedBooks => [...prevSelectedBooks, bookId]);
+        } else {
+            setSelectedBooks(prevSelectedBooks =>
+                prevSelectedBooks.filter(id => id !== bookId)
+            );
+            setSelectedUnits(prevSelectedUnits =>
+                prevSelectedUnits.filter(unitId =>
+                    units.find(unit => unit._id === unitId).bookId !== bookId
+                )
+            );
         }
-    }
+    };
+    const handleUnitCheckboxChange = (event, unitId) => {
+        if (event.target.checked) {
+            setSelectedUnits(prevSelectedUnits => [...prevSelectedUnits, unitId]);
+        } else {
+            setSelectedUnits(prevSelectedUnits =>
+                prevSelectedUnits.filter(id => id !== unitId)
+            );
+        }
+    };
+
     return (
         <>
             <div className="row">
-                <div className="col-lg-4 col-md-4 col-sm-12">
-                    <select name="" id="" className="form-control" onChange={(e) => {
-                        handlerBook(e);
-                    }}>
-                        <option selected disabled>select book</option>
-                        {
-                            books.map((book) => {
-                                return <>
-                                    <option className="form-control">{book.bookname}</option>
-                                </>
-                            })
-                        }
-                    </select>
+                <div>
+                    <h2>Kitoblar</h2>
                 </div>
                 <div className="col-lg-4 col-md-4 col-sm-12">
-                    <select name="" id="" className="form-control" onChange={(e) => {
-                        handlerUnit(e);
-                    }}>
-                        <option selected disabled>select units</option>
-                        {
-                            // bookId.length ? bookId.map(bookid => {
-                            //     units.map(unit => {
-                            //         if (unit.bookId == bookid) {
-                            //             // return <><option >{ unit.unitname}</option></>  
-                            //             return <>
-                            //                 <option value={unit.unitname}> unit.unitname </option>
-                            //             </>
-                            //         }
-                            //     })
-                            // }) : ""
-                        }
-                    </select>
+                    {books.map(book => (
+                        <li key={book._id}>
+                            <h3>{book.bookname.substring(30, 1000)}</h3>
+                            <input
+                                type="checkbox"
+                                checked={selectedBooks.includes(book._id)}
+                                onChange={event => handleBookCheckboxChange(event, book._id)}
+                            />
+                        </li>
+                    ))}
+                </div>
+                <div className="col-lg-4 col-md-4 col-sm-12">
+                    <h2>Unitlar</h2>
+
+                    {units
+                        .filter(unit => selectedBooks.includes(unit.bookId))
+                        .map(unit => (
+                            <li key={unit._id}>
+                                <h3>{unit.unitname}</h3>
+                                <p>{unit.description}</p>
+                                <input
+                                    type="checkbox"
+                                    checked={selectedUnits.includes(unit._id)}
+                                    onChange={event => handleUnitCheckboxChange(event, unit._id)}
+                                />
+                            </li>
+                        ))}
                 </div>
             </div>
+            <p>qancha soz bilan randomli o'yin o'ynaysiz. taxminan {count ? count : wordsCount} bilan o'ynamoqchimisiz. bu ko'pmasmi?</p>
+            <input type="range" max={wordsCount} maxLength={wordsCount}  onChange={(e) => {
+                setCount(e.target.value);
+            }} />
         </>
     );
 };
